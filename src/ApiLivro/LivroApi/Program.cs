f5937bfaa49ca88e6e90061e0f8ad4e95ffa4420
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-20 20:12:14 -0300

    feat: configura injeção de dependencia

--- src/ApiLivro/LivroApi/Program.cs
+++ src/ApiLivro/LivroApi/Program.cs
@@ -1,6 +1,11 @@
+using LivroApi.Data.Repository;
+using LivroApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ILivroServices, LivroServices>();
+builder.Services.AddScoped<ILivroRepository, LivroRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
