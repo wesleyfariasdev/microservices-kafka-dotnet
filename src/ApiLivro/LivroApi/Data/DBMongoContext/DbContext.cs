9e4e3b74d80d90848f80ea3016e5bd27b97c3d99
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-20 18:41:07 -0300

    feat: adiciona context com mongoDb

---
+++ src/ApiLivro/LivroApi/Data/DBMongoContext/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using LivroApi.Data.Settings;
+using LivroApi.Domain;
+using MongoDB.Driver;
+
+namespace LivroApi.Data.DBMongoContext;
+
+public sealed class DbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public DbContext(MongoDbSettings settings)
+    {
+        var client = new MongoClient(settings.ConnectionString);
+        _database = client.GetDatabase(settings.DatabaseName);
+    }
+
+    public IMongoCollection<Livro> Livros => _database.GetCollection<Livro>("Livros");
+} 
---
+++ src/ApiLivro/LivroApi/Data/Settings/MongoDbSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace LivroApi.Data.Settings;
+
+public sealed class MongoDbSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+}
