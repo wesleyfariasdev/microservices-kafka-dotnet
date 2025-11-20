fd728cb259fdfc73b2fafdfe80c71d79fa5d0bbc
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-20 20:11:35 -0300

    feat: adiciona request e response

---
+++ src/ApiLivro/LivroApi/Dto/Response/LivroResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using LivroApi.Domain;
+
+namespace LivroApi.Dto.Response;
+
+public sealed class LivroResponseDto
+{
+    public string Id { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string ISBN { get; set; }
+    public int AnoPublicacao { get; set; }
+    public Situacao SituacaoLivro { get; set; }
+    public Guid UsuarioId { get; set; }
+}
