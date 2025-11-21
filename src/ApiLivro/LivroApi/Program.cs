c7c41fc6573b0e8fed274518e617aa7fd9be18d9
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-21 00:41:01 -0300

    fix: corrige injeção de dependencia

--- src/ApiLivro/LivroApi/Program.cs
+++ src/ApiLivro/LivroApi/Program.cs
@@ -1,11 +1,19 @@
+using LivroApi.Data.DBMongoContext;
 using LivroApi.Data.Repository;
+using LivroApi.Data.Settings;
 using LivroApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add config MongoDb
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<DbContext>();
+
 // Add services to the container.
-builder.Services.AddScoped<ILivroServices, LivroServices>();
 builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+builder.Services.AddScoped<ILivroServices, LivroServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
