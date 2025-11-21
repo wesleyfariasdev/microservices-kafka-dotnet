e0f4b043951397277b13393ff03d463969fce03a
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-21 00:41:32 -0300

    style code: remove espaços

--- src/ApiLivro/LivroApi/Dto/Request/LivroRequestDto.cs
+++ src/ApiLivro/LivroApi/Dto/Request/LivroRequestDto.cs
@@ -1,15 +1,13 @@
 ﻿using LivroApi.Domain;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace LivroApi.Dto.Request;
 
 public sealed class LivroRequestDto
 {
-    public string Titulo { get;  set; }
-    public string Autor { get;  set; }
-    public string ISBN { get;  set; }
-    public int AnoPublicacao { get;  set; } 
-    public Situacao SituacaoLivro { get;  set; } 
-    public Guid UsuarioId { get;  set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string ISBN { get; set; }
+    public int AnoPublicacao { get; set; }
+    public Situacao SituacaoLivro { get; set; }
+    public Guid UsuarioId { get; set; }
 }
