c6c7d14d50fb572bbd4a2b9163bca9a914050e64
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-21 23:17:08 -0300

    feat: adiciona e configura swagger

--- src/ApiLivro/LivroApi/Program.cs
+++ src/ApiLivro/LivroApi/Program.cs
@@ -16,15 +16,16 @@ builder.Services.AddScoped<ILivroRepository, LivroRepository>();
 builder.Services.AddScoped<ILivroServices, LivroServices>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
