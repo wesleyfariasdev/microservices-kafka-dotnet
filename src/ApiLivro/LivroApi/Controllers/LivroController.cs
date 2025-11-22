14456165e29f0bbbf962dc2f449ea9c4883b2aa9
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-22 12:02:06 -0300

    feat: implementa controllers

--- src/ApiLivro/LivroApi/Controllers/LivroController.cs
+++ src/ApiLivro/LivroApi/Controllers/LivroController.cs
@@ -1,10 +1,33 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LivroApi.Dto.Request;
+using LivroApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LivroApi.Controllers;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
-public class LivroController : ControllerBase
+public class LivroController(ILivroServices livroServices) : ControllerBase
 {
+    [HttpGet]
+    public async Task<IActionResult> ObterLivros()
+    {
+        var livros = await livroServices.ObterTodosLivros();
+        if (livros is null)
+            return NotFound();
+        
+        return Ok(livros);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> CriarLivro([FromBody] LivroRequestDto livroDto)
+    {
+        var livroCriado = await livroServices.Criar(livroDto);
+        if (livroCriado is null)
+            return BadRequest();
+
+        return CreatedAtAction(nameof(ObterLivros), new { id = livroCriado.Id }, livroCriado);
+    }
 }
