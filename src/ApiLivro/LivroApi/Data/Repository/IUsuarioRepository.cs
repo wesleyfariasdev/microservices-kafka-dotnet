4b71c3f2bd3bc47bbb2fb60515ba98f940e79bdb
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-11-22 00:33:56 -0300

    feat: implementa cadastro de usuários

---
+++ src/ApiLivro/LivroApi/Data/Repository/IUsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using LivroApi.Domain;
+
+namespace LivroApi.Data.Repository;
+
+public interface IUsuarioRepository
+{
+    Task<bool> CriarUsuarioAsync(Usuario usuario);
+    Task<bool> VerificarUsuarioCadastradoPorEmailAsync(string email);
+    Task<Usuario> ObterUsuarioPorEmailAsync(string email);
+}
---
+++ src/ApiLivro/LivroApi/Data/Repository/UsuarioRepository.cs
@@ -0,0 +1,20 @@
+﻿using LivroApi.Data.DBMongoContext;
+using LivroApi.Domain;
+using MongoDB.Driver;
+
+namespace LivroApi.Data.Repository;
+
+public class UsuarioRepository(DbContext context) : IUsuarioRepository
+{
+    public async Task<bool> CriarUsuarioAsync(Usuario usuario)
+    {
+        await context.Usuarios.InsertOneAsync(usuario);
+        return true;
+    }
+
+    public async Task<Usuario> ObterUsuarioPorEmailAsync(string email) =>
+           await context.Usuarios.Find(u => u.Email == email).FirstAsync();
+
+    public async Task<bool> VerificarUsuarioCadastradoPorEmailAsync(string email) =>
+           await context.Usuarios.Find(u => u.Email == email).AnyAsync();
+}
